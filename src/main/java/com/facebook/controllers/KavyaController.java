0c7735b9e38bced6b5802174f5e1a55a3395d08e
dinakaravera <dinakaravera@Dinakaravera>
2018-07-14 12:45:16 +0530

    KavyaController implemented

---
+++ src/main/java/com/facebook/controllers/KavyaController.java
@@ -0,0 +1,9 @@
+package com.facebook.controllers;
+
+public class KavyaController {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello....! welcome KavyaController");
+	}
+
+}
