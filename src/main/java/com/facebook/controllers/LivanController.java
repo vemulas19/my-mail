abdaa7edb0283ad4d444f7e233f16663e4adb598
Rahulbodapatla <Rahulbodapatla@RahulBodapatla>
2018-07-14 12:40:46 +0530

    Livan_Controller flow implemented.....!

---
+++ src/main/java/com/facebook/controllers/LivanController.java
@@ -0,0 +1,12 @@
+package com.facebook.controllers;
+
+public class LivanController {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Loging conttroler is Done.......!!!");
+
+	}
+
+}
