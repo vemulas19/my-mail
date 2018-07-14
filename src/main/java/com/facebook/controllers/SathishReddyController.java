58d0abd6b99feeabdc164311451f7d452c73ed2b
laddusiddu <laddusiddu@LAXMI>
2018-07-14 12:35:47 +0530

    sathish reddy controller

---
+++ src/main/java/com/facebook/controllers/SathishReddyController.java
@@ -0,0 +1,12 @@
+package com.facebook.controllers;
+
+public class SathishReddyController {
+	public static void main(String[] args) {
+		
+
+		System.out.println("hai every one");
+		System.out.println("hello every one");
+		
+	}
+
+}
