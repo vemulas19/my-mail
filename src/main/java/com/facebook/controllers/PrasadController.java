77da1fb21c6678a4599fddf5fcb8dadbba261865
laddusiddu <laddusiddu@LAXMI>
2018-07-14 12:42:33 +0530

    prasad ctrl is done

---
+++ src/main/java/com/facebook/controllers/PrasadController.java
@@ -0,0 +1,10 @@
+package com.facebook.controllers;
+
+public class PrasadController {
+	
+	public static void main(String[] args) {
+		System.out.println("entering into prasadctrl");
+		System.out.println("exiting into prasadctrl");
+	}
+
+}
