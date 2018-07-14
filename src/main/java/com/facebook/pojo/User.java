5da3a93434fed717e4bf03de9ff7733be3ccddb5
hp <hp@DESKTOP-1L03BVT>
2018-07-14 18:46:01 +0530

    done with review comments

--- src/main/java/com/facebook/pojo/User.java
+++ src/main/java/com/facebook/pojo/User.java
@@ -7,13 +7,14 @@ package com.facebook.pojo;
  */
 public class User {
 
-	private String userName;
+	private String name;
 	private String password;
-	public String getUserName() {
-		return userName;
+	
+	public String getName() {
+		return name;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setName(String name) {
+		this.name = name;
 	}
 	public String getPassword() {
 		return password;
