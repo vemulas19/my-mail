fb5c6c6350f9e352dfc63ddab31b0d78343ddaac
suresh surya <suresh surya@LAPTOP-BCL0HMPG>
2018-07-14 18:26:36 +0530

    profiles added

---
+++ src/main/java/com/facebook/pojo/Profiles.java
@@ -0,0 +1,26 @@
+package com.facebook.pojo;
+
+/**
+ * 
+ * @author suresh surya
+ *
+ */
+public class Profiles {
+
+	private String profileName;
+	private String profileValidity;
+	
+	public String getProfileName() {
+		return profileName;
+	}
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+	public String getProfileValidity() {
+		return profileValidity;
+	}
+	public void setProfileValidity(String profileValidity) {
+		this.profileValidity = profileValidity;
+	}
+	
+}
