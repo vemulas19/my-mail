54d8c8f183bc4e7fd1c3dd5e6b322f05023ddc90
suresh surya <suresh surya@LAPTOP-BCL0HMPG>
2018-07-14 11:45:57 +0530

    registration implemented!!

---
+++ src/main/java/com/facebook/controllers/RegistrationController.java
@@ -0,0 +1,9 @@
+package com.facebook.controllers;
+
+public class RegistrationController {
+
+	public static void main(String[] args) {
+		System.out.println("Registration is started");
+		System.out.println("Registration is done");
+	}
+}
