e9165346740e8a314eb81bfcae0599bd6f658378
Lenovo <Lenovo@NAGAMANI>
2018-07-14 13:03:10 +0530

    nagamani controller done

---
+++ src/main/java/com/facebook/controllers/NagamaniController.java
@@ -0,0 +1,10 @@
+package com.facebook.controllers;
+
+public class NagamaniController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("nagamani controller");
+	}
+
+}
