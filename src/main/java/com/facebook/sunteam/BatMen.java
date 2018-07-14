e04ba2275b1d58b83ded2fd42a24bcb4bf7233d6
Samsung <Samsung@Samsung-PC>
2018-07-14 22:18:42 +0530

    Done........

---
+++ src/main/java/com/facebook/sunteam/BatMen.java
@@ -0,0 +1,37 @@
+package com.facebook.sunteam;
+/**
+ * 
+ * @author sagar
+ *
+ */
+public class BatMen {
+
+	private String playerName;
+	private String playerJursyNumber;
+	private String playerRole;
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public String getPlayerJursyNumber() {
+		return playerJursyNumber;
+	}
+
+	public void setPlayerJursyNumber(String playerJursyNumber) {
+		this.playerJursyNumber = playerJursyNumber;
+	}
+
+	public String getPlayerRole() {
+		return playerRole;
+	}
+
+	public void setPlayerRole(String playerRole) {
+		this.playerRole = playerRole;
+	}
+
+}
