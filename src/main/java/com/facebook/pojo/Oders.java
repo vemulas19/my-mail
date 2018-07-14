382c310e62534c0710b8b9faa922117164c6aa8f
Rahulbodapatla <Rahulbodapatla@RahulBodapatla>
2018-07-14 19:14:29 +0530

    oders commited

---
+++ src/main/java/com/facebook/pojo/Oders.java
@@ -0,0 +1,45 @@
+package com.facebook.pojo;
+
+/**
+ * @author Rahulbodapatla
+ *
+ */
+public class Oders {
+
+	private Integer oser_id;
+	private String oder_name;
+	private String oder_price;
+	private String oder_location;
+	
+	
+	
+	
+	public Integer getOser_id() {
+		return oser_id;
+	}
+	public void setOser_id(Integer oser_id) {
+		this.oser_id = oser_id;
+	}
+	public String getOder_name() {
+		return oder_name;
+	}
+	public void setOder_name(String oder_name) {
+		this.oder_name = oder_name;
+	}
+	public String getOder_price() {
+		return oder_price;
+	}
+	public void setOder_price(String oder_price) {
+		this.oder_price = oder_price;
+	}
+	public String getOder_location() {
+		return oder_location;
+	}
+	public void setOder_location(String oder_location) {
+		this.oder_location = oder_location;
+	}
+	
+	
+	
+	
+}
