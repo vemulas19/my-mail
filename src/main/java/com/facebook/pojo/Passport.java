de0b9c2e2f9e07fc47893c934b620bc71178c471
ABHINAV GOUD <ABHINAV GOUD@ABHI>
2018-07-19 12:32:29 +0530

    passport

---
+++ src/main/java/com/facebook/pojo/Passport.java
@@ -0,0 +1,23 @@
+package com.facebook.pojo;
+
+public class Passport {
+	
+
+	private String name;
+	private String Panno;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPanno() {
+		return Panno;
+	}
+	public void setPanno(String panno) {
+		Panno = panno;
+	}
+	
+	
+}
