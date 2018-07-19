c1521033227100d3e2e38fecdcd92f6658c3d66d
ABHINAV GOUD <ABHINAV GOUD@ABHI>
2018-07-19 13:18:26 +0530

    added

--- src/main/java/com/facebook/pojo/Passport.java
+++ src/main/java/com/facebook/pojo/Passport.java
@@ -4,19 +4,28 @@ public class Passport {
 	
 
 	private String name;
-	private String Panno;
+	private String Passnumber;
+	private String git;
 	
+	
+	
+	public String getGit() {
+		return git;
+	}
+	public void setGit(String git) {
+		this.git = git;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getPanno() {
-		return Panno;
+	public String getPassnumber() {
+		return Passnumber;
 	}
-	public void setPanno(String panno) {
-		Panno = panno;
+	public void setPassnumber(String passnumber) {
+		Passnumber = passnumber;
 	}
 	
 	
