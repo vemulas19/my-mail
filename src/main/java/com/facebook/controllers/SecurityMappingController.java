dcbbc39c0763a12670355c6c34b79dc7da765d97
hp <hp@DESKTOP-1L03BVT>
2018-07-27 09:07:36 +0530

    ioc

---
+++ src/main/java/com/facebook/controllers/SecurityMappingController.java
@@ -0,0 +1,31 @@
+package com.facebook.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecurityMappingController {
+
+	final static Logger logger = Logger.getLogger(SecurityMappingController.class);
+	
+	@RequestMapping(value="/gotoLoginPage")
+	public String openLoginPage() {
+		logger.info("Entered into openLoginPage :: method");
+		return "login";
+	}
+	
+	@RequestMapping(value="/gotoFailurePage")
+	public String openLoginPageForFail(Model model) {
+		logger.info("Entered into openLoginPage :: method");
+		model.addAttribute("message", "Invalid Credentials!! Please try again!!");
+		return "login";
+	}
+	
+	@RequestMapping(value="/home")
+	public String gotoHome() {
+		logger.info("Entered into openLoginPage :: method");
+		return "Home";
+	}
+}
